40e935b132b65c2002931909f53613b3aae41507
Abdalrahman <abdalrahmandaas963@gmail.com>
2023-06-26 15:18:41 +0300

    mockend added

--- src/api/kanbanApi.js
+++ src/api/kanbanApi.js
@@ -1,8 +1,12 @@
 import axios from "axios";
 
 const Api = axios.create({
-  baseURL: "http://localhost:3000/",
+  baseURL: "https://my-json-server.typicode.com/AbdAlrahmanDaas963/json-data/",
 });
+
+// const Api = axios.create({
+//   baseURL: "http://localhost:3000/",
+// });
 
 export const getBoard = async (boardId) => {
   if (!boardId) return null;
